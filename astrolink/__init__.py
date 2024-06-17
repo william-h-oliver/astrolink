5b45f99c581e2d0e71fa6755b10a2dfa1f818995
william-h-oliver <william.oliver@iwr.uni-heidelberg.de>
2024-06-17 14:41:37 +0200

    Force JIT compilation on import

--- astrolink/__init__.py
+++ astrolink/__init__.py
@@ -1,3 +1,8 @@
 # The version file is generated automatically by setuptools_scm
 from astrolink._version import version as __version__
 from .astrolink import AstroLink
+
+# Force JIT compilation on import
+import numpy as np
+c = AstroLink(np.random.uniform(0, 1, (500, 3)))
+c.run()

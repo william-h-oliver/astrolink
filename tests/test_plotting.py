6defcaae45a1cb3f31573b79b8786720e5bae2d0
william-h-oliver <william.oliver@iwr.uni-heidelberg.de>
2024-12-13 16:43:37 +0100

    Add unit tests for plotting functions in test_plotting.py to validate AstroLink visualizations

---
+++ tests/test_plotting.py
@@ -0,0 +1,24 @@
+import numpy as np
+from astrolink import AstroLink
+from astrolink import plotting
+
+
+
+def test_plotting():
+    # Run with float64 data in 2-dimensions
+    gauss2D_1 = np.random.normal(0, 1, (10**4, 2)) # Two gaussian blobs
+    gauss2D_2 = np.random.normal([10, 0], 1, (10**4, 2))
+    P = np.concatenate((gauss2D_1, gauss2D_2), axis = 0)
+    
+    try: clusterer = AstroLink(P, verbose = 2)
+    except: assert False, "AstroLink class could not be instantiated"
+
+    try: clusterer.run()
+    except: assert False, "AstroLink.run() could not be run with input-data type np.float64"
+
+    # Plotting
+    try: line, polyCollections = plotting.plotOrderedDensity(clusterer)
+    except: assert False, "AstroLink.plotOrderedDensity() could not be run"
+
+    try: h, bins, patches, line, lineCollection = plotting.plotProminenceModel(clusterer)
+    except: assert False, "AstroLink.plotProminenceModel() could not be run"
